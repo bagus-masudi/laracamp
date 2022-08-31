b2858e85aabec9734473706218d7f80ec5443b1f
Ahmad Bagus Mas'udi <ahmadbagusmasudi@gmail.com>
2022-08-31 14:11:24 +0700

    seeders

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Gila Belajar',
+                'slug' => 'gila-belajar',
+                'price' => 200,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+            [
+                'title' => 'Baru Mulai',
+                'slug' => 'baru-mulai',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time()),
+            ],
+        ];
+
+        // 1st method
+        Camp::insert($camps);
+
+        /** 2st method (cara ini mengisi otomatis created_at dan updated_at)
+         *  foreach ($camps as $key => $camp){
+         *      Camp::create($camp)
+         *  }
+         * */ 
+        
+    }
+}
